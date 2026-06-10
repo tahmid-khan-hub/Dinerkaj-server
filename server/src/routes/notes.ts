9f8a9fa16a37b7f9d614179e207c6afe9444a230
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-06-10 16:07:21 +0600

    Update the post api of notes routes

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -7,12 +7,12 @@ router.post("/", async(req: Request, res: Response) => {
     const userId = req.user?.id;
     if (!userId) return res.status(401).json({ error: "Unauthorized" });
 
-    const { notesTitle, category, notesDescription } = req.body;
-    if (!notesTitle) return res.status(400).json({ error: "Notes title is required" });
+    const { title, category, description } = req.body;
+    if (!title) return res.status(400).json({ error: "Notes title is required" });
 
     try {
         const newNote = await pool.query(`
-            INSERT INTO notes (user_id, notesTitle, category, notesDescription) VALUES ($1, $2, $3, $4) RETURNING *`, [userId, notesTitle, category, notesDescription]);
+            INSERT INTO notes (user_id, title, category, description) VALUES ($1, $2, $3, $4) RETURNING *`, [userId, title, category, description]);
         res.status(201).json(newNote.rows[0]);
     } catch (error) {
         res.status(500).json({ error: "Failed to create new note" });
